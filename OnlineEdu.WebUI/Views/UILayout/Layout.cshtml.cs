5525d249e3dc97d265167d79f92b3c19af67a03f
İbrahim SARIASLAN <ibrahim.sariaslan123@hotmail.com>
2026-05-18 19:50:50 +0300

    .net 9.0'a projenın geri cekilme işlemi

---
+++ OnlineEdu.WebUI/Views/UILayout/Layout.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OnlineEdu.WebUI.Views.UILayout
+{
+    public class LayoutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
